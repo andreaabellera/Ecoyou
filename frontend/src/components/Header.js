bdd3b1d607efef429c85777b0f895f45e8685a3f
A.C. Abellera <58119036+viamajor@users.noreply.github.com>
2021-01-16 01:04:19 -0600

    Header new design
    > Header still in progress, React props are hard lol
    > Croco is our new icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import "../styles/Header.css";
 
-import ecoyouLogo from "../resources/ecoyou.png";
+import HeaderBase from "../resources/header_base.png";
+import HeaderOrnament from "../resources/header_ornament.png";
+import EcoyouLogo from "../resources/ecoyou.png";
+import HeaderDiscoverIcon from "../resources/header_discover.png";
+import HeaderRedeemIcon from "../resources/header_redeem.png";
+import HeaderChallengesIcon from "../resources/header_challenges.png";
+import HeaderMeIcon from "../resources/header_me.png";
 
-import HeaderDiscoverIcon from "../resources/header-discover.png";
-import HeaderRedeemIcon from "../resources/header-redeem.png";
-import HeaderChallengesIcon from "../resources/header-challenges.png";
-import HeaderMeIcon from "../resources/header-me.png";
+import HeaderDiscoverSelected from "../resources/header_discover_selected.png";
+import HeaderRedeemSelected from "../resources/header_redeem_selected.png";
+import HeaderChallengesSelected from "../resources/header_challenges_selected.png";
+import HeaderMeSelected from "../resources/header_me_selected.png";
 
 import { 
     BrowserRouter, NavLink, Route, Switch, useLocation
@@ -24,31 +30,49 @@ function Header()
 
     React.useEffect(() =>
     {
-        
+        document.title = "Ecoyou";
     }, [location]);
 
     return (
         <header>
-            <NavLink to="/discover" className="header-item" activeClassName="active">
-                <img src={HeaderDiscoverIcon} className="tab-icon" />
-                Discover
-            </NavLink>
-            <NavLink to="/redeem" className="header-item" activeClassName="active">
-                <img src={HeaderRedeemIcon} className="tab-icon" />
-                Redeem
-            </NavLink>
-            <div className="logo-container"> 
-                <img src={ecoyouLogo} className="header-logo" />
-                <span>The change starts with you</span>
+            <div className = "header-base" style={{ backgroundImage: `url(${HeaderBase})` }}>
+                <div className = "header-item">
+                    <NavLink to="/discover" className="header-item" activeClassName="active">
+                        <img src={HeaderDiscoverIcon} />
+                        Discover
+                    </NavLink>
+                </div>
+
+                <div className = "header-item">
+                    <NavLink to="/challenges" className="header-item" activeClassName="active">
+                        <img src={HeaderChallengesIcon} />
+                        Challenges
+                    </NavLink>
+                </div>
+
+                <div className = "header-item">
+                    <NavLink to="/about" className="header-item" activeClassName="active">
+                        <img src={EcoyouLogo} />
+                        The change starts with you
+                    </NavLink>
+                </div>
+
+                <div className = "header-item">
+                    <NavLink to="/redeem" className="header-item" activeClassName="active">
+                        <img src={HeaderRedeemIcon} />
+                        Redeem
+                    </NavLink>
+                </div>
+
+                <div className = "header-item">
+                    <NavLink to="/me" className="header-item" activeClassName="active">
+                        <img src={HeaderMeIcon} />
+                        Me
+                    </NavLink>
+                </div>
             </div>
-            <NavLink to="/challenges" className="header-item" activeClassName="active">
-                <img src={HeaderChallengesIcon} className="tab-icon" />
-                My challenges
-            </NavLink>
-            <NavLink to="/me" className="header-item" activeClassName="active">
-                <img src={HeaderMeIcon} className="tab-icon" />
-                Me
-            </NavLink>
+
+            <div className="header-ornament" style={{ backgroundImage: `url(${HeaderOrnament})` }}></div>
         </header>
     );
 }
